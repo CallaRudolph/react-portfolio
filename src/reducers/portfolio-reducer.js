2df4235c2e99a6bc6bc7bf470fffecb4380358c1
Calla Rudolph <callarudolph@gmail.com>
2017-09-29 10:28:40 -0700

    modify reducer to pass third test for displayPortfolio

--- src/reducers/portfolio-reducer.js
+++ src/reducers/portfolio-reducer.js
@@ -2,6 +2,7 @@ import constants from './../constants';
 const { defaultState, types } = constants;
 
 const portfolio = (state = defaultState, action) => {
+  let portfolio;
   let newPortfolio;
   let newState;
   switch (action.type) {
@@ -15,10 +16,19 @@ const portfolio = (state = defaultState, action) => {
       });
       return newState;
     case types.DISPLAY_PORTFOLIO:
-      newPortfolio = {
-
-      };
-      newState = [];
+      portfolio = state[action.portfolioId];
+      newPortfolio = Object.assign({}, portfolio, {
+        isFetching: false,
+        url: action.url,
+        name: action.name,
+        blog: action.blog,
+        location: action.location,
+        repos: action.repos,
+        portfolioId: action.portfolioId
+      });
+      newState = Object.assign({}, state, {
+        [action.portfolioId]: newPortfolio
+      });
       return newState;
     default:
       return state;
