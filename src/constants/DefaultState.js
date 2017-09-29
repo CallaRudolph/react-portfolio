fb8456bb9b34a1ee9fcc0bfc267b7ff6d5f3d547
Calla Rudolph <callarudolph@gmail.com>
2017-09-29 09:53:07 -0700

    create constants folder with first action type and default state

---
+++ src/constants/DefaultState.js
@@ -0,0 +1,9 @@
+export const defaultState = {
+  isFetching: false,
+  url: "https://api/github.com",
+  name: "Portfolio",
+  blog: "https://www.blog.com",
+  location: "Buffalo, NY",
+  repos: "0",
+  portfolioId: 0
+}
