221c74a1f90953c905363c9934e75855d4a11890
Calla Rudolph <callarudolph@gmail.com>
2017-09-29 10:57:26 -0700

    write out fetch method to capture API response

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,8 +17,25 @@ export const displayPortfolio = (url, name, blog, location, repos, portfolioId) 
 });
 
 export function fetchPortfolio() {
-  console.log("hello");
   return function (dispatch) {
     const portfolioId = v4();
-  }
+    dispatch(requestPortfolio(portfolioId));
+    return fetch("https://api.github.com/users/CallaRudolph")
+    .then(
+      response => response.json(),
+      error => console.log("An error occurred.", error)
+    ).then(function(json) {
+      console.log(json);
+      if (json.id > 1) {
+        const url = json.html_url;
+        const name = json.name;
+        const blog = json.blog;
+        const location = json.location;
+        const repos = json.public_repos;
+        dispatch(displayPortfolio(url, name, blog, location, repos, portfolioId));
+      } else {
+        console.log("error");
+      }
+    });
+  };
 }
