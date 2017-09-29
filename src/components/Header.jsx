be3eb23038a0f29aad0ca297dd29a504c24897cd
Calla Rudolph <callarudolph@gmail.com>
2017-09-29 13:07:48 -0700

    add navbar styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 function Header(){
+  var link = {
+    paddingTop: "10",
+    paddingRight: "5"
+  }
   return (
-    <div>
-      <Link to="/">Portfolio</Link> | <Link to="/images">Images</Link>
+    <div className="navbar navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <h3>Portfolio</h3>
+          <p><em>Thanks, <a href="https://developer.github.com/v3/">GitHub API</a></em></p>
+        </div>
+        <div style={link} className="nav navbar-nav navbar-right">
+          <Link to="/">Portfolio</Link> | <Link to="/images">Images</Link>
+        </div>
+      </div>
     </div>
   );
 }
